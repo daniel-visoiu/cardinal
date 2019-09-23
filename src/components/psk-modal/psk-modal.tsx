05824137eaf11f2f956200f2ffa7fd0a9b3ae934
Your Name <elena.vizitiu.ev@gmail.com>
2019-09-23 17:25:19 +0300

    Small update on psk-modal

--- src/components/psk-modal/psk-modal.tsx
+++ src/components/psk-modal/psk-modal.tsx
@@ -1,57 +1,50 @@
-import {Component, h, Prop, Event, EventEmitter, Element} from '@stencil/core';
+import { Component, h, Prop, Event, EventEmitter } from '@stencil/core';
 
 @Component({
-  tag: 'psk-modal',
-  styleUrl: './psk-modal.css',
-  shadow: true
+	tag: 'psk-modal',
+	styleUrl: './psk-modal.css',
+	shadow: true
 })
 export class PskModal {
-  @Element() element: HTMLElement;
+	@Prop({ reflectToAttr: true, mutable: true }) opened: boolean = false;
 
-  @Prop({reflectToAttr: true, mutable: true}) opened: boolean = false;
+	@Event({
+		eventName: 'closeModal',
+		composed: true,
+		cancelable: true,
+		bubbles: true,
+	}) closeModal: EventEmitter;
 
-  @Event({
-    eventName: 'closeModal',
-    composed: true,
-    cancelable: true,
-    bubbles: true,
-  }) closeModal: EventEmitter;
+	render() {
+		return (
+			<div>
+				<div id="backdrop" onClick={() => {
+					this.closeModal.emit();
+				}} />
+				<div id="modal">
+					<div class="modal-content">
+						<div class="modal-header">
+							<slot name="title" />
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true"
+									onClick={() => {
+										this.closeModal.emit();
+									}}>&times;</span>
+							</button>
+						</div>
 
-  render() {
-    return (
-      <div>
-        <div id="backdrop" onClick={() => {
-  this.closeModal.emit();
-}}/>
-        <div id="modal">
-
-          <div class="modal-content">
-
-            <div class="modal-header">
-              <slot name="title"/>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true" onClick={() => {
-                  this.closeModal.emit();
-                }}>&times;</span>
-              </button>
-            </div>
-
-            <div class="modal-body">
-              <slot/>
-            </div>
-
-            <div class="modal-footer">
-              <slot name="confirm_action"/>
-              <wg-button
-                label="Close"
-                buttonClass="btn btn-primary"
-                eventEmitter={this.closeModal}
-              />
-            </div>
-
-          </div>
-        </div>
-      </div>
-    );
-  }
+						<div class="modal-body">
+							<slot />
+						</div>
+						<div class="modal-footer">
+							<slot name="confirm_action" />
+							<button class="btn btn-primary" onClick={() => {
+								this.closeModal.emit();
+							}}>Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
