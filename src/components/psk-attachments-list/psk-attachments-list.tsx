b20655a7cb8e98bec640ba8d960adaf65d2d99b4
Paius Paraschiv-Catalin <p.cata95@gmail.com>
2019-10-05 14:54:58 +0300

    small improvements for attach file and file chooser

--- src/components/psk-attachments-list/psk-attachments-list.tsx
+++ src/components/psk-attachments-list/psk-attachments-list.tsx
@@ -1,49 +1,53 @@
-import {Component, h, Prop} from '@stencil/core';
-import {WgFile} from "../../interfaces/WgFile";
+import { Component, h, Prop } from '@stencil/core';
+import { WgFile } from "../../interfaces/WgFile";
 const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
 
 @Component({
-  tag: 'psk-attachments-list',
-  styleUrl: './psk-attachments-list.css',
-  shadow: true
+	tag: 'psk-attachments-list',
+	styleUrl: './psk-attachments-list.css',
+	shadow: true
 })
 
 export class PskAttachmentsList {
-  @Prop() files: WgFile[] = [];
+	@Prop() files: WgFile[] = [];
 
-  static bytesToSize(bytes) {
-    if (bytes == 0) return '0 Byte';
-    let sizeIndex = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))));
-    return Math.round(bytes / Math.pow(1024, sizeIndex)) + ' ' + sizes[sizeIndex];
-  }
+	static bytesToSize(bytes) {
+		if (bytes == 0) return '0 Byte';
+		let sizeIndex = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))));
+		return Math.round(bytes / Math.pow(1024, sizeIndex)) + ' ' + sizes[sizeIndex];
+	}
 
-  render() {
-    let filesView = this.files.map((file) => {
+	render() {
+		if (!this.files || this.files.length === 0) {
+			return <h5>No attachments available!</h5>;
+		}
 
-      let fileType = null;
-      switch (file.name.substr(file.name.lastIndexOf(".") + 1)) {
-        case "pdf":
-          fileType = "fa-file-pdf-o";
-          break;
-        case "xls":
-          fileType = "fa-file-excel-o";
-          break;
-        case "doc":
-          fileType = "fa-file-word-o";
-          break;
-        case "jpg":
-        case "png":
-          fileType = "fa-file-picture-o";
-          break;
-        default:
-          fileType = "fa-file-o";
-      }
+		let filesView = this.files.map((file) => {
 
-      return <button type="button" class="btn btn-primary mr-1 mt-1">
-        <span class={`icon mr-1 fa ${fileType}`}/>{file.name}
-        <span class="badge badge-light ml-1">{PskAttachmentsList.bytesToSize(file.size)}</span></button>
-    });
+			let fileType = null;
+			switch (file.name.substr(file.name.lastIndexOf(".") + 1)) {
+				case "pdf":
+					fileType = "fa-file-pdf-o";
+					break;
+				case "xls":
+					fileType = "fa-file-excel-o";
+					break;
+				case "doc":
+					fileType = "fa-file-word-o";
+					break;
+				case "jpg":
+				case "png":
+					fileType = "fa-file-picture-o";
+					break;
+				default:
+					fileType = "fa-file-o";
+			}
 
-    return (filesView)
-  }
+			return <button type="button" class="btn btn-primary mr-2 mt-2">
+				<span class={`icon mr-1 fa ${fileType}`} />{file.name}
+				<span class="badge badge-light ml-1">{PskAttachmentsList.bytesToSize(file.size)}</span></button>
+		});
+
+		return (filesView)
+	}
 }
--- src/components/psk-files-chooser/psk-files-chooser.tsx
+++ src/components/psk-files-chooser/psk-files-chooser.tsx
@@ -1,45 +1,48 @@
-import {Component, h, Prop} from '@stencil/core';
-import {WgFile} from "../../interfaces/WgFile";
+import { Component, h, Prop } from '@stencil/core';
+import { WgFile } from "../../interfaces/WgFile";
 
 @Component({
-  tag: 'psk-files-chooser',
-  styleUrl: './psk-files-chooser.css',
-  shadow: true
+	tag: 'psk-files-chooser',
+	styleUrl: './psk-files-chooser.css',
+	shadow: true
 })
 
 export class PskFilesChooser {
-  @Prop() label: string = "Select files";
-  @Prop() accept?: string;
-  @Prop() onFilesChange?: Function;
-  @Prop() onFilesSelect?: Function;
+	@Prop() label: string = "Select files";
+	@Prop() accept?: string;
+	@Prop() onFilesChange?: Function;
+	@Prop() onFilesSelect?: Function;
 
-  addedFile(event) {
+	addedFile(event) {
 
-    let filesArray = Array.from(event.target.files);
-    if (typeof this.onFilesChange === "function") {
-      this.onFilesChange(filesArray);
-    }
+		let filesArray = Array.from(event.target.files);
+		if (typeof this.onFilesChange === "function") {
+			this.onFilesChange(filesArray);
+		}
 
-    if (typeof this.onFilesSelect === "function") {
-      let files: WgFile[] = filesArray.map((attachment: any) => {
+		if (typeof this.onFilesSelect === "function") {
+			let files: WgFile[] = filesArray.map((attachment: any) => {
 
-        return {
-          name: attachment.name,
-          size: attachment.size,
-          type: attachment.name.substr(attachment.name.lastIndexOf(".") + 1)
-        };
-      });
-      this.onFilesSelect(files);
-    }
-  }
+				return {
+					name: attachment.name,
+					size: attachment.size,
+					type: attachment.name.substr(attachment.name.lastIndexOf(".") + 1)
+				};
+			});
+			this.onFilesSelect(files);
+		}
+	}
 
-  render() {
-    return (<button type="button" class="btn btn-secondary p-0">
-      <label class="pt-0 mb-0 p-2">
-        {this.label}
-        <input accept={this.accept} type="file" onChange={this.addedFile.bind(this)} multiple
-               class="form-control-file form-control-sm"/>
-      </label>
-    </button>)
-  }
+	render() {
+		return [
+			<button type="button" class="btn btn-secondary p-0">
+				<label class="pt-0 mb-0 p-2">
+					{this.label}
+					<input accept={this.accept} type="file" onChange={this.addedFile.bind(this)} multiple
+						class="form-control-file form-control-sm" />
+				</label>
+			</button>,
+			!this.onFilesChange && !this.onFilesSelect ? <h5 class="mt-4">No controller set for this component!</h5> : null
+		]
+	}
 }
