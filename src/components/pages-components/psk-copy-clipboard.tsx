36eba4064c58a13492f261cdb9072c04fa2ab38b
Paius Paraschiv-Catalin <p.cata95@gmail.com>
2019-10-25 15:00:59 +0300

    fixed focus on chapter when clicking on copy to clipboard button

--- src/components/pages-components/psk-copy-clipboard.tsx
+++ src/components/pages-components/psk-copy-clipboard.tsx
@@ -13,27 +13,14 @@ export class PskCard {
 
     _copyToClipboardHandler(elementId: string): void {
         try {
-            const pskPageElement = closestParentElement(getElement(this), 'psk-page');
-
             let basePath = window.location.href;
             if (window.location.href.indexOf("?chapter=") !== -1) {
                 basePath = window.location.href.split("?chapter=")[0];
             }
 
-            const clipboardText = `${basePath}?chapter=${elementId}`;
+            navigator.clipboard.writeText(`${basePath}?chapter=${elementId}`);
 
-            const copyInput: HTMLInputElement = document.createElement('input');
-            pskPageElement.appendChild(copyInput);
-            copyInput.setAttribute('value', clipboardText);
-
-            copyInput.select();
-            copyInput.setSelectionRange(0, 99999);
-
-            document.execCommand("copy");
-
-            scrollToElement(elementId, pskPageElement);
-            // this.element.querySelector('#tooltip').innerHTML = TOOLTIP_COPIED_TEXT
-            pskPageElement.removeChild(copyInput);
+            scrollToElement(elementId, closestParentElement(getElement(this), 'psk-page'));
         } catch (err) {
             console.error(err);
         }
@@ -54,7 +41,7 @@ export class PskCard {
 
     render() {
 
-        const elementId = this.id.trim().replace(/( |:)/g,"-").toLowerCase();
+        const elementId = this.id.trim().replace(/( |:|\/)/g, "-").toLowerCase();
         if (elementId.length === 0 || !this._isCopySupported()) {
             return;
         }
