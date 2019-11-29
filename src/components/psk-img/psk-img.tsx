1fca25b3f21e0cbe1bde4e1b331d8bd870b6f75c
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-11-29 18:49:05 +0200

    simplified the psk-img tag

--- src/components/psk-img/psk-img.tsx
+++ src/components/psk-img/psk-img.tsx
@@ -1,8 +1,6 @@
-import { Component, h, Prop, State } from "@stencil/core";
+import { Component, h, Prop } from "@stencil/core";
 import CustomTheme from "../../decorators/CustomTheme";
 import { TableOfContentProperty } from "../../decorators/TableOfContentProperty";
-
-const defaultBasePath = "https://raw.githubusercontent.com/PrivateSky/pwc-apps/master/pages/";
 
 @Component({
 	tag: "psk-img"
@@ -26,28 +24,11 @@ export class PskImg {
 	})
 	@Prop() title: string;
 
-	@State() basePath: string;
-
-	constructor() {
-		if (this.src.indexOf('http') !== -1) {
-			this.basePath = this.src;
-			return;
-		}
-
-		// @ts-ignore
-		if (typeof globalConfig !== "undefined" && typeof globalConfig.pagesBasePath === "string") {
-			// @ts-ignore
-			this.basePath = globalConfig.pagesBasePath + this.src;
-		} else {
-			this.basePath = defaultBasePath + this.src;
-		}
-	}
-
 	render() {
 		return (
 			<div class="image_container">
 				<div class="image_wrapper">
-					<img src={this.basePath} class="img-fluid" alt={this.title} />
+					<img src={this.src} class="img-fluid" alt={this.title} />
 				</div>
 				{this.title ? <div class="image_description">{this.title}</div> : null}
 			</div>
