ed8e46b8faa3840777d765bebb5223edef8e485a
paiusCatalin <p.cata95@gmail.com>
2020-05-08 12:06:03 +0300

    refactor psk-checkbox - display the label the the surrounding html only if label has value

--- src/components/form-components/psk-checkbox/psk-checkbox.tsx
+++ src/components/form-components/psk-checkbox/psk-checkbox.tsx
@@ -16,28 +16,33 @@ export class PskCheckbox {
         const checkboxName = this.name ? this.name
             : this.checkboxLabel ? this.checkboxLabel.replace(/\s/g, '').toLowerCase() : '';
 
-        return (
+        const checkboxHtml = (
             <div class="form-group">
-                {/* Here, we display the label of the grouped checkbox component. Details in the documentation */}
-                <psk-label label={this.label} />
-
-                <div class="form-group">
-                    <div class="form-check form-check-inline">
-                        <input
-                            type="checkbox"
-                            class="form-check-input"
-                            id={checkboxName}
-                            name={checkboxName}
-                            required={this.required}
-                            checked={this.checked}
-                            onChange={this.__handleCheckbox.bind(this)}
-                            value={this.value} />
-                        {/* This is the label for the checkbox */}
-                        <psk-label for={checkboxName} label={this.checkboxLabel} />
-                    </div>
+                <div class="form-check form-check-inline">
+                    <input
+                        type="checkbox"
+                        class="form-check-input"
+                        id={checkboxName}
+                        name={checkboxName}
+                        required={this.required}
+                        checked={this.checked}
+                        onChange={this.__handleCheckbox.bind(this)}
+                        value={this.value} />
+                    {/* This is the label for the checkbox */}
+                    <psk-label for={checkboxName} label={this.checkboxLabel} />
                 </div>
             </div>
         );
+        
+        return this.label
+            ? (
+                <div class="form-group">
+                    {/* Here, we display the label of the grouped checkbox component. Details in the documentation */}
+                    <psk-label label={this.label} />
+                    {checkboxHtml}
+                </div>
+            )
+            : checkboxHtml;
     }
 
     __handleCheckbox(evt): void {
