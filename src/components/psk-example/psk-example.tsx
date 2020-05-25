9e9fc6ded4ee0281a9f445368efdd2e2bc2dc7bb
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-05-25 23:10:33 +0300

    added style to psk-example component

--- src/components/psk-example/psk-example.tsx
+++ src/components/psk-example/psk-example.tsx
@@ -1,11 +1,15 @@
 import { Component, h, Prop } from "@stencil/core";
 import { TableOfContentProperty } from "../../decorators/TableOfContentProperty";
+import CustomTheme from "../../decorators/CustomTheme";
 
 @Component({
-	tag: "psk-example"
+	tag: "psk-example",
+  shadow:true
 })
 
 export class PskExample {
+  @CustomTheme()
+
 	@TableOfContentProperty({
 		description:`The title of the component's example that will be used to create a psk-chapter.`,
 		isMandatory:false,
@@ -16,7 +20,9 @@ export class PskExample {
 	render() {
 		return (
 			<psk-chapter title={this.title}>
-				<slot />
+        <div class="example-content">
+				  <slot />
+        </div>
 			</psk-chapter>
 		);
 	}
