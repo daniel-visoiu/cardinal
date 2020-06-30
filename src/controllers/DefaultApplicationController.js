1c5327a9cb227d38f9c1bb758a94e22881f6e8c9
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-06-30 16:38:09 +0300

    split app-config.json into 2 separate files: config.json and menu.json

--- src/controllers/DefaultApplicationController.js
+++ src/controllers/DefaultApplicationController.js
@@ -1,6 +1,7 @@
 import AppConfigurationHelper from "./AppConfigurationHelper.js";
 import defaultApplicationConfig from "./defaultApplicationConfig.json";
-let configUrl = "app-config.json";
+let configUrl = "config.json";
+let menuUrl = "menu.json";
 window.globalConfig = {};
 export default class DefaultApplicationController  {
 
@@ -24,21 +25,36 @@ export default class DefaultApplicationController  {
         basePath += "/";
       }
       configUrl = basePath + configUrl;
+      menuUrl = basePath + menuUrl;
       window.basePath = basePath;
       this._getAppConfiguration(configUrl, (err, _configuration) => {
 
+        if (err) {
+          return console.log(err);
+        }
+
+        this._fetchConfigurationFile(menuUrl, (err, menuConfig) => {
+
+          if(err){
+             console.log(err);
+          }
+          else{
+            _configuration.menu = menuConfig;
+          }
+
           this.configuration = AppConfigurationHelper._prepareConfiguration(_configuration, basePath);
           this.configuration.theme = _configuration.theme;
-            this.configuration.appVersion = _configuration.appVersion;
-            this.configIsLoaded = true;
-            while (this.pendingRequests.length) {
-                let request = this.pendingRequests.pop();
-                if (!this.configuration[request.configName]) {
-                    throw new Error(`Config ${request.configName} was not provided. Did you set it in app-config.json?`)
-                }
-                request.callback(undefined, this.configuration[request.configName]);
+          this.configuration.appVersion = _configuration.appVersion;
+          this.configIsLoaded = true;
+          while (this.pendingRequests.length) {
+            let request = this.pendingRequests.pop();
+            if (!this.configuration[request.configName]) {
+              throw new Error(`Config ${request.configName} was not provided. Did you set it in app-config.json?`)
             }
+            request.callback(undefined, this.configuration[request.configName]);
+          }
         });
+      })
 
         element.addEventListener("getThemeConfig", this._provideConfig("theme"));
         element.addEventListener("getAppVersion", this._provideConfig("appVersion"));
@@ -134,29 +150,31 @@ export default class DefaultApplicationController  {
         }
     }
 
-    _getAppConfiguration(url, callback) {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.onload = () => {
-            if (xhr.status != 200) {
-                console.error("Configuration file app-config.json was not found!");
-                callback(null, defaultApplicationConfig);
+  _getAppConfiguration(url, callback) {
+    this._fetchConfigurationFile(url, (err, configuration) => {
+      if (err) {
+        console.log(err);
+        //use default configuration
+        return callback(undefined, defaultApplicationConfig);
+      }
 
-            } else {
-                let customConfiguration = JSON.parse(xhr.responseText);
+      for (let i in defaultApplicationConfig) {
+        if (!configuration.hasOwnProperty(i)) {
+          configuration[i] = defaultApplicationConfig[i];
+        }
+      }
+      callback(undefined, configuration)
 
-              for (let i in defaultApplicationConfig) {
-                if (!customConfiguration.hasOwnProperty(i)) {
-                  customConfiguration[i] = defaultApplicationConfig[i];
-                }
-              }
-                callback(null, customConfiguration)
-            }
-        };
+    });
+  }
 
-        xhr.onerror = () => {
-            callback(new Error("An error occurred"));
-        };
-        xhr.send();
-    }
+  _fetchConfigurationFile(url, callback) {
+    fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      callback(undefined, data);
+    }).catch(function (e) {
+      callback(e);
+    });
+  }
 }
