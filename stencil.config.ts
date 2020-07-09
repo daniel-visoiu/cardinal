bb30cb7f23dede5f94b1d1a921464ea39d32f140
paval victor <pavalvictor98@yahoo.com>
2020-07-09 07:37:12 +0300

    created build-prod script + added remove_warnings_plugin

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,36 +1,40 @@
-  import { Config } from '@stencil/core';
+import './bin/remove_warnings_plugin.js'
+import { Config } from '@stencil/core';
 
-  export interface CardinalConfig extends Config{
-    readonly useBootstrap: boolean;
-  }
+export interface CardinalConfig extends Config {
+  readonly useBootstrap: boolean;
+}
 
-  export const config: CardinalConfig = {
-    namespace: 'cardinal',
-    outputTargets: [
-      {
-        type: 'dist',
-        esmLoaderPath: '../loader',
-        copy: [
-          { src: 'controllers/AppConfigurationHelper.js', dest:"../cardinal/controllers/AppConfigurationHelper.js", warn:true },
-          { src: 'controllers/base-controllers', dest:"../cardinal/controllers/base-controllers", warn:true },
-          { src: 'events/*.js', dest:"../cardinal/events", warn:true }
-        ]
-      },
-      {
-        type: 'docs-readme'
-      },
-      {
-        type: 'www',
-        serviceWorker: null // disable service workers
-      }
-    ],
-    buildEs5: false,
-    extras: {
-      cssVarsShim: false,
-      dynamicImportShim: false,
-      safari10: false,
-      scriptDataOpts: false,
-      shadowDomShim: false
+export const config: CardinalConfig = {
+  namespace: 'cardinal',
+  plugins: [
+    global.removeWarnings()
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+      copy: [
+        { src: 'controllers/AppConfigurationHelper.js', dest: "../cardinal/controllers/AppConfigurationHelper.js", warn: true },
+        { src: 'controllers/base-controllers', dest: "../cardinal/controllers/base-controllers", warn: true },
+        { src: 'events/*.js', dest: "../cardinal/events", warn: true }
+      ]
     },
-    useBootstrap:true
-  };
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ],
+  buildEs5: false,
+  extras: {
+    cssVarsShim: false,
+    dynamicImportShim: false,
+    safari10: false,
+    scriptDataOpts: false,
+    shadowDomShim: false
+  },
+  useBootstrap: true
+};
