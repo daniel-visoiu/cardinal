bed4fce4e6d2f9d5fbba100e92fb97709def1d8f
skutner <sava.dumitru.daniel@gmail.com>
2020-09-07 16:02:45 +0300

    changed 127.0.0.1 to localhost

--- bin/build_dossier.js
+++ bin/build_dossier.js
@@ -1,5 +1,5 @@
 const DOSSIER_SEED_FILE_PATH = "./seed";
-const BRICK_STORAGE_ENDPOINT = process.env.SSAPPS_FAVORITE_EDFS_ENDPOINT || "http://127.0.0.1:8080";
+const BRICK_STORAGE_ENDPOINT = process.env.SSAPPS_FAVORITE_EDFS_ENDPOINT || "http://localhost:8080";
 
 require("./../../privatesky/psknode/bundles/csbBoot.js");
 require("./../../privatesky/psknode/bundles/edfsBar.js");
