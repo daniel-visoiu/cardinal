3be6af3a9b315749393a24f47f0c490c6241ed51
Deny Pătrașcu <denypatrascu@gmail.com>
2020-10-08 15:07:59 +0300

    psk-user-profile-renderer update

--- src/components/psk-user-profile/psk-user-profile-renderer.tsx
+++ src/components/psk-user-profile/psk-user-profile-renderer.tsx
@@ -1,30 +1,36 @@
-import {Component, h, Prop,} from '@stencil/core';
-import { TableOfContentProperty } from '../../decorators/TableOfContentProperty';
+import { Component, Prop, h } from '@stencil/core';
+
 import CustomTheme from '../../decorators/CustomTheme';
-import {BindModel} from '../../decorators/BindModel';
+import { BindModel } from '../../decorators/BindModel';
+import { TableOfContentProperty } from '../../decorators/TableOfContentProperty';
 
 @Component({
   tag: 'psk-user-profile-renderer',
-  styleUrl:"../../assets/css/bootstrap/bootstrap.css"
+  styleUrl: "../../assets/css/bootstrap/bootstrap.css",
+  shadow: true
 })
+
 export class PskUserProfileRenderer {
+  @CustomTheme()
 
   @BindModel() modelHandler;
-  @CustomTheme()
+
   @TableOfContentProperty({
     description: `This property is the user information that needs to be rendered for the user.`,
     isMandatory: false,
     propertyType: `any`
   })
-  @Prop() userInfo:any;
+  @Prop() userInfo: any;
 
   render() {
+    const { avatar, username, email } = this.userInfo;
+
     return (
       <div class="profile">
         <div class="card-body text-center">
-          <p><img src={this.userInfo.avatar} alt="card image"/></p>
-          <h5 class="card-title">{this.userInfo.username}</h5>
-          <p class="card-text">{this.userInfo.email}</p>
+          <p><img src={avatar} alt="card image" /></p>
+          <h5 class="card-title">{username}</h5>
+          <p class="card-text">{email}</p>
         </div>
       </div>
     );
