56a202c586a1e454c1000503cd98e2f9d0d6d738
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-30 19:30:51 +0200

    Added cardinal namespace to window. Ensure classes that used local cache to now use a global cache instead.

--- src/services/ControllerRegistryService.js
+++ src/services/ControllerRegistryService.js
@@ -1,46 +1,48 @@
+window.cardinal = window.cardinal || {};
+window.cardinal.controllers = window.cardinal.controllers || {};
+window.cardinal.pendingControllerRequests =
+  window.cardinal.pendingControllerRequests || {};
+
+const { controllers, pendingControllerRequests } = window.cardinal;
+
 class ControllerRegistryService {
-    constructor() {
-        this.controllers = {};
-        this.pendingControllerRequests = {}
-    }
+  registerController(controllerName, controller) {
+    controllers[controllerName] = controller;
+    this._fullFillPreviousRequests(controllerName);
+  }
 
-    registerController(controllerName, controller) {
-        this.controllers[controllerName] = controller;
-        this._fullFillPreviousRequests(controllerName);
+  _fullFillPreviousRequests(controllerName) {
+    if (pendingControllerRequests[controllerName]) {
+      while (pendingControllerRequests[controllerName].length) {
+        let request = pendingControllerRequests[controllerName].pop();
+        request.resolve(controllers[controllerName]);
+      }
     }
+  }
 
-    _fullFillPreviousRequests(controllerName) {
-        if (this.pendingControllerRequests[controllerName]) {
-            while (this.pendingControllerRequests[controllerName].length) {
-                let request = this.pendingControllerRequests[controllerName].pop();
-                request.resolve(this.controllers[controllerName]);
-            }
+  getController(controllerName) {
+    let controllerPromise = new Promise((resolve, reject) => {
+      if (controllers[controllerName]) {
+        resolve(controllers[controllerName]);
+      } else {
+        let resourcePath = `scripts/controllers/${controllerName}.js`;
+        if (typeof window.basePath !== "undefined") {
+          let sep = "/";
+          if (window.basePath[window.basePath.length - 1] === sep) {
+            sep = "";
+          }
+          resourcePath = window.basePath + sep + resourcePath;
         }
-    }
+        import(resourcePath)
+          .then((module) => {
+            resolve(module.default || module);
+          })
+          .catch(reject);
+      }
+    });
 
-    getController(controllerName) {
-        let controllerPromise = new Promise((resolve, reject) => {
-
-            if (this.controllers[controllerName]) {
-                resolve(this.controllers[controllerName]);
-            } else {
-                let resourcePath = `scripts/controllers/${controllerName}.js`;
-                if(typeof window.basePath !== "undefined"){
-                    let sep = "/";
-                    if(window.basePath[window.basePath.length-1] ===sep){
-                        sep = "";
-                    }
-                    resourcePath = window.basePath+sep+resourcePath;
-                }
-                import (resourcePath)
-                .then((module) => {
-                    resolve(module.default || module);
-                }).catch(reject);
-            }
-        });
-
-        return controllerPromise;
-    }
+    return controllerPromise;
+  }
 }
 
-export default new ControllerRegistryService();
+export default ControllerRegistryService;
